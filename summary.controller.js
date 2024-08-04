8c459cc41f2860fecfb402a7b2a0f45b1cf7e22d
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2024-08-04 13:19:30 +0300

    Update summary.controller.js
    Examining controllers

--- summary.controller.js
+++ summary.controller.js
@@ -1,15 +1,34 @@
 (function () {
   'use strict'
-
-angular
+  
+  angular
   .module('horizon.app.core.images')
   .controller('horizon.app.core.images.DrawerController', controller);
 
-controller.$inject = [
-  'horizon.app.core.openstack-service.api.glance',
-  'horizon.app.core.images.resourceType'
-];
+  controller.$inject = [
+  'horizon.app.core.openstack-service.api.glance', //glance
+  'horizon.app.core.images.resourceType' //imageResourceType
+  ];
 
-}
+  var metadataDefs;
+
+  function controller(glance, imageResourceType){
+    var ctrl = this; // an instance of the controller
+
+    ctrl.metadataDefs = metadatDefs;
+
+    if(!ctrl.metadataDefs){
+      applyMetadataDefinitions();
+    {
+
+    function applyMetadataDefinitions(){
+      glance.getNamespaces({resource_type:imagesResourceType}, true)
+        .then(function setMetadefs(data){
+          ctrl.metadataDefs = data.data.items;
+        });
+    }            
+  }
+})();
+ 
 
   
