bcdfe9b27ce11d1b35ff5aceaefa7bc6f0b6da16
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2024-08-14 14:03:29 +0300

    Update and rename deactivate.js to deactivate.html
    Attempt 2

--- deactivate.js
+++
@@ -1,8 +0,0 @@
-            <div class="form-field">
-              <div class="btn-group" name="status">
-                <label class="btn btn-default"
-                       ng-repeat="option in ctrl.imageStatusOptions"
-                       ng-model="ctrl.image.status"
-                       uib-btn-radio="option.value">{$ ::option.label $}</label>
-              </div>
-            </div>
