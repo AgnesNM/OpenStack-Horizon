83279a79bdefd42d258d360eba712addf15abc7c
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2024-08-18 12:14:55 +0300

    Update deactivate.js
    Showing the radio buttons

--- deactivate.js
+++ deactivate.js
@@ -25,3 +25,20 @@ getStatuses();
       { label: gettext('Yes'), value: true },
       { label: gettext('No'), value: false }
     ];
+
+// Attempt 3
+
+ctrl.imageStatusOptions = [
+      { label: gettext('Active'), value: 'active' },
+    ];
+
+// Using the rules defined in openstack_dashboard/conf/glance_policy.yaml
+
+    function getStatuses() {
+      policy.ifAllowed({rules: [['image', 'deactivate']]}).then(
+        function() {
+          ctrl.imageStatusOptions.push({ label: gettext('Deactivated'), value: 'deactivated' });
+        }
+      );
+    }
+
