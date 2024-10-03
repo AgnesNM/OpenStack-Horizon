e208ce1f629b3d6de74aa50c85985a5a4bedb777
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2024-10-03 13:03:01 +0300

    Create Zuul errors.py

---
+++ Zuul errors.py
@@ -0,0 +1,17 @@
+Firefox 115.0 (Linux x86_64) horizon.app.core.images.actions.edit.service perform submit passes the image from the model to updateImage FAILED
+	TypeError: deactivated is undefined in static/app/core/images/actions/edit.action.service.js (line 21)
+	submit@static/app/core/images/actions/edit.action.service.js:103:11 <- static/app/core/images/actions/edit.action.service.js:21:1156
+	@static/app/core/images/actions/edit.action.service.spec.js:133:21
+	<Jasmine>
+Firefox 115.0 (Linux x86_64): Executed 75 of 1152 (1 FAILED) (0 secs / 0.785 secs)
+Firefox 115.0 (Linux x86_64) horizon.app.core.images.actions.edit.service perform submit returns a failed result if API call fails FAILED
+	TypeError: deactivated is undefined in static/app/core/images/actions/edit.action.service.js (line 21)
+	submit@static/app/core/images/actions/edit.action.service.js:103:11 <- static/app/core/images/actions/edit.action.service.js:21:1156
+	@static/app/core/images/actions/edit.action.service.spec.js:142:34
+	<Jasmine>
+Firefox 115.0 (Linux x86_64): Executed 76 of 1152 (2 FAILED) (0 secs / 0.798 secs)
+Firefox 115.0 (Linux x86_64) horizon.app.core.images.actions.editWorkflow create the workflow for editing image FAILED
+	Expected 3 to equal 2.
+	<Jasmine>
+	@static/app/core/images/actions/edit.workflow.service.spec.js:47:28
+	<Jasmine>
